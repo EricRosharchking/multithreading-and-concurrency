cc7f955e672691914dde01bc6da44a02040d3548
Liyuan Hong <liyuan_hong@epam.com>
2022-03-02 10:54:07 +0800

    Finished Task2 with continuous adding numbers to list, while printing sum and square root of sum of all squares.

---
+++ src/main/java/com/liyuan/hong/globaljavamentor/Task2.java
@@ -0,0 +1,124 @@
+package com.liyuan.hong.globaljavamentor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Create three threads:
+ * <p>
+ * 1st thread is infinitely writing random number to the collection;
+ * 2nd thread is printing sum of the numbers in the collection;
+ * 3rd is printing square root of sum of squares of all numbers in the collection.
+ * Make these calculations thread-safe using synchronization block. Fix the possible deadlock.
+ */
+public class Task2 {
+    static List<Integer> list;
+    public static AtomicBoolean status = new AtomicBoolean(true);
+
+    public static void main(String[] args) {
+        list = new ArrayList<Integer>();
+        Thread t1 = new Thread(new WriteNumbersRunnable(list));
+        Thread t2 = new Thread(new SumNumbersRunnable(list));
+        Thread t3 = new Thread(new SquareRootNumbersRunnable(list));
+        t1.start();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t3.start();
+    }
+}
+
+class WriteNumbersRunnable implements Runnable {
+    private List<Integer> list;
+    private Random rd;
+    private boolean status;
+
+    public WriteNumbersRunnable(List<Integer> list) {
+        this.list = list;
+        rd = new Random();
+        status = false;
+    }
+
+    public void run() {
+        synchronized (list) {
+            while (Task2.status.get()) {
+                list.add(rd.nextInt(1000));
+                System.out.println("add and sleep");
+                try {
+                    Thread.sleep(123);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class SumNumbersRunnable implements Runnable {
+    private List<Integer> list;
+    private int count;
+    private int sum;
+
+    public SumNumbersRunnable(List<Integer> list) {
+        this.list = list;
+        count = 0;
+        sum = 0;
+    }
+
+    public void run() {
+//        synchronized (list) {
+        while (Task2.status.get()) {
+            while (count < list.size()) {
+                sum += list.get(count++);
+                System.out.println("Sum is " + sum);
+                try {
+                    Thread.sleep(450);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+//        }
+
+}
+
+class SquareRootNumbersRunnable implements Runnable {
+    private List<Integer> list;
+    private int count;
+    private int sumOfSquares;
+
+    public SquareRootNumbersRunnable(List<Integer> list) {
+        this.list = list;
+        count = 0;
+        sumOfSquares = 0;
+    }
+
+    public void run() {
+//        synchronized (list) {
+        while (Task2.status.get()) {
+            while (count < list.size()) {
+                int i = list.get(count++);
+                sumOfSquares += i * i;
+                System.out.println("SquareRoot is " + Math.sqrt(sumOfSquares));
+                try {
+                    Thread.sleep(550);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+//        }
+    }
+}
